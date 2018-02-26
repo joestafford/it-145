33967b00558157e8cc051154b06c359176a55cd2
Joe Stafford <joey.stafford@snhu.edu>
2018-02-25 21:13:18 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-package zoo;
-
 import java.util.Scanner;
 import java.security.MessageDigest;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 
 public class Main {
  
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     Authentication userSession = new Authentication();
     Role userRole = new Role();
     
@@ -16,5 +15,10 @@ public class Main {
     userRole.printRole(userSession.sessionRole);
     
   }
+
+private static void userSession() {
+	// TODO Auto-generated method stub
+	
+}
   
 }
